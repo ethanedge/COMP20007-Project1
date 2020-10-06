a2747c6c4a0d4ad8376c76b8e07a63c360fb3e25
ethanedge <68106387+ethanedge@users.noreply.github.com>
2020-10-06 20:55:19 +1100

    Uploaded the project from my computer

---
+++ provided_files/deque.c
@@ -0,0 +1,317 @@
+/* * * * * * *
+ * Deque module (i.e., double ended queue) for Assignment 1
+ *
+ * created for COMP20007 Design of Algorithms 2020
+ * template by Tobias Edwards <tobias.edwards@unimelb.edu.au>
+ * implementation by Ethan Edge
+ */
+
+// You must not change any of the code already provided in this file, such as
+// type definitions, constants or functions.
+//
+// You may, however, add additional functions and/or types which you may need
+// while implementing your algorithms and data structures.
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "deque.h"
+#include "util.h"
+
+#define INITIAL 5
+
+// Create a new empty Deque and return a pointer to it
+Deque *new_deque() {
+	Deque *deque = malloc(sizeof(*deque));
+	assert(deque);
+
+	deque->top = NULL;
+	deque->bottom = NULL;
+	deque->size = 0;
+
+	return deque;
+}
+
+// Free the memory associated with a Deque
+void free_deque(Deque *deque) {
+  // Remove (and thus free) all of the nodes in the Deque.
+  while (deque->size > 0) {
+    deque_remove(deque);
+  }
+
+	// Free the deque struct itself
+	free(deque);
+}
+
+// Create a new Node with a given piece of data
+Node *new_node(Data data) {
+  Node *node = malloc(sizeof(*node));
+  assert(node);
+
+  node->next = NULL;
+  node->prev = NULL;
+  node->data = data;
+
+  return node;
+}
+
+// Free the memory associated with a Node
+void free_node(Node *node) {
+  free(node);
+}
+
+// Add an element to the top of a Deque
+void deque_push(Deque *deque, Data data) {
+  Node *new = new_node(data);
+
+  if (deque->size > 0) {
+    new->next = deque->top;
+    deque->top->prev = new;
+  } else {
+    // If the Deque was initially empty then new is both the top and bottom
+    deque->bottom = new;
+  }
+
+  deque->top = new;
+  deque->size++;
+}
+
+// Add an element to the bottom of a Deque
+void deque_insert(Deque *deque, Data data) {
+  Node *new = new_node(data);
+
+  if (deque->size > 0) {
+    new->prev = deque->bottom;
+    deque->bottom->next = new;
+  } else {
+    // If the Deque was initially empty then new is both the top and bottom
+    deque->top = new;
+  }
+
+  deque->bottom = new;
+  deque->size++;
+}
+
+// Remove and return the top element from a Deque
+Data deque_pop(Deque *deque) {
+  if (deque->size == 0) {
+    exit_with_error("can't pop from empty Deque");
+  }
+
+  Data data = deque->top->data;
+  Node *old_top = deque->top;
+
+  if (deque->size == 1) {
+    deque->top = NULL;
+    deque->bottom = NULL;
+  } else {
+    deque->top = old_top->next;
+    deque->top->prev = NULL;
+  }
+
+  deque->size--;
+
+  free(old_top);
+
+  return data;
+}
+
+// Remove and return the bottom element from a Deque
+Data deque_remove(Deque *deque) {
+  if (deque->size == 0) {
+    exit_with_error("can't remove from empty Deque");
+  }
+
+  Data data = deque->bottom->data;
+  Node *old_bottom = deque->bottom;
+
+  if (deque->size == 1) {
+    deque->top = NULL;
+    deque->bottom = NULL;
+  } else {
+    deque->bottom = old_bottom->prev;
+    deque->bottom->next = NULL;
+  }
+
+  deque->size--;
+
+  free(old_bottom);
+
+  return data;
+}
+
+// Return the number of elements in a Deque
+int deque_size(Deque *deque) {
+  return deque->size;
+}
+
+// Print the Deque on its own line with the following format:
+//   [x_1, x_2, ..., x_n]
+//     ^              ^
+//    top           bottom
+void print_deque(Deque *deque) {
+  Node *current = deque->top;
+  int i = 0;
+
+  printf("[");
+
+  while (current) {
+    printf("%d", current->data);
+    // Print a comma unless we just printed the final element
+    if (i < deque->size - 1) {
+      printf(", ");
+    }
+    current = current->next;
+    i++;
+  }
+
+  printf("]\n");
+}
+
+// Reverse the Deque using an iterative approach
+void iterative_reverse(Deque *deque) {
+
+  /* Takes in a pointer to a deque, uses an iterative approach in order */
+  /* to reverse the deque's content's. */
+  /* Alters the original deque using its pointers, returns nothing. */
+
+  if (deque->size == 0) {
+    exit_with_error("can't reverse empty Deque");
+  }
+
+  Node *temp; // holds next pointer temporarily
+  Node *traversal = deque->top;  // our main traversal node
+
+  while (traversal != NULL) {
+    /* Changes node's pointers */
+    temp = traversal->next;
+    traversal->next = traversal->prev;
+    traversal->prev = temp;
+
+    /* Move to the next node */
+    traversal = temp;
+  }
+
+  /* Reverse top/bottom pointers */
+  temp = deque->top;
+  deque->top = deque->bottom;
+  deque->bottom = temp;
+
+}
+
+// Reverse the Deque using a recursive approach
+void recursive_reverse(Deque *deque) {
+
+  /* Takes in a pointer to a deque, uses a recursive approach in order */
+  /* to reverse the deque's content's. */
+  /* Alters the original deque using its pointers, returns nothing. */
+
+  if (deque->size == 0) {
+    exit_with_error("can't reverse empty Deque");
+  }
+
+    /* Holds our next and traversal node pointers */
+    Node *temp = deque->top;
+    Node *traversal = deque->top;
+
+    /* Starts the recursive call to reverse the nodes' pointers */
+    recursive_reverse_sub(traversal, temp);
+
+    /* Reverse top/bottom pointers */
+    temp = deque->top;
+    deque->top = deque->bottom;
+    deque->bottom = temp;
+}
+
+// Reverse the Deque using a recursive approach
+void recursive_reverse_sub(Node *traversal, Node *temp) {
+
+    /* The sub function that is called recursively for recursive_reverse. */
+    /* Takes in a current traversal node and a temp node in order to */
+    /* reverse the 'prev' and 'next' pointers of the node, returns nothing. */
+
+    /* Serves as a base case, stops when we get to the end of the deque, */
+    /* otherwise keeps traversing recursively. */
+    if (traversal != NULL) {
+        /* Changes node's pointers */
+        temp = traversal->next;
+        traversal->next = traversal->prev;
+        traversal->prev = temp;
+
+        /* Calls the function again */
+        recursive_reverse_sub(temp, temp);
+    }
+}
+
+// Split the Deque given a critical value k, such that the Deque contains
+// all elements greater than equal to k above (i.e., closer to the top)
+// the elements less than k.
+//
+// Within the two parts of the array (>= k and < k) the elements should
+// be in their original order.
+//
+// This function must run in linear time.
+
+void split_deque(Deque *deque, int k) {
+
+  int curr_great, curr_less;         /* Current items inside array */
+  curr_great = curr_less = 0;
+
+  int tot_great, tot_less;           /* Total array size */
+  tot_great = tot_less = INITIAL;
+
+  int *less_k, *great_k;             /* Arrays to hold our numbers */
+
+  less_k = malloc(tot_less*sizeof(*less_k));
+  assert(less_k != NULL);
+  great_k = malloc(tot_great*sizeof(*great_k));
+  assert(great_k != NULL);
+
+  Node *temp = deque->top;
+  int i;
+
+  if (deque->size == 0) {
+    exit_with_error("can't reverse empty Deque");
+  }
+
+ /* Sorts our deque numbers into less than or greater than or equal to arrays */
+ for(i = 0; i < deque->size; i++) {
+    if (temp->data < k) {
+      if (curr_less == (tot_less - 1)) {
+        tot_less *= 2;
+        less_k = realloc(less_k, tot_less*sizeof(*less_k));
+      }
+      less_k[curr_less++] = temp->data;
+    }
+    else {
+      if (curr_great == (tot_great - 1)) {
+        tot_great *= 2;
+        great_k = realloc(great_k, tot_great*sizeof(*great_k));
+      }
+      great_k[curr_great++] = temp->data;
+    }
+
+    temp = temp->next;
+  }
+
+  /* Goes through both arrays and puts their values into the deque */
+  /* now in the required order. */
+  temp = deque->top;
+
+ for(i = 0; i < curr_great; i++) {
+    temp->data = great_k[i];
+    temp = temp->next;
+ }
+
+  for(i = 0; i < curr_less; i++) {
+    temp->data = less_k[i];
+    temp = temp->next;
+  }
+
+ free(less_k);
+ free(great_k);
+
+}
+
+// TODO: Add any other functions you might need for your Deque module
---
+++ provided_files/parkranger.c
@@ -0,0 +1,259 @@
+/* * * * * * *
+ * Park Ranger module for Assignment 1
+ *
+ * created for COMP20007 Design of Algorithms 2020
+ * template by Tobias Edwards <tobias.edwards@unimelb.edu.au>
+ * implementation by Ethan Edge
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "parkranger.h"
+#include "util.h"
+#include "deque.h"
+
+#define NUM_ROOTS     1
+#define ROOT_NODE     0
+#define UNEXPLORED    false
+
+// This function must read in a ski slope map and determine whether or not
+// it is possible for the park ranger to trim all of the trees on the ski slope
+// in a single run starting from the top of the mountain.
+//
+// The ski slope map is provided via stdin in the following format:
+//
+//   n m
+//   from to
+//   from to
+//   ...
+//   from to
+//
+// Here n denotes the number of trees that need trimming, which are labelled
+// {1, ..., n}. The integer m denotes the number "reachable pairs" of trees.
+// There are exactly m lines which follow, each containing a (from, to) pair
+// which indicates that tree `to` is directly reachable from tree `from`.
+// `from` and `to` are integers in the range {0, ..., n}, where {1, ..., n}
+// denote the trees and 0 denotes the top of the mountain.
+//
+// For example the following input represents a ski slope with 3 trees and
+// 4 reachable pairs of trees.
+//
+// input:            map:          0
+//   3 4                          / \
+//   0 1                         /  2
+//   0 2                        / /
+//   2 1                        1
+//   1 3                          \
+//                                 3
+//
+// In this example your program should return `true` as there is a way to trim
+// all trees in a single run. This run is (0, 2, 1, 3).
+//
+// Your function should must:
+//  - Read in this data from stdin
+//  - Store this data in an appropriate data structure
+//  - Run the algorithm you have designed to solve this problem
+//  - Do any clean up required (e.g., free allocated memory)
+//  - Return `true` or `false` (included in the stdbool.h library)
+//
+// For full marks your algorithm must run in O(n + m) time.
+bool is_single_run_possible() {
+
+  /* Creates an adjacency list and populates it */
+  int n;
+  Deque **adj_list = read_in_input(&n);
+
+  /* Perform a depth-first search to get a topological ordering */
+  Deque *topological_sort = new_deque();
+  if(topological_DFS(adj_list, topological_sort, n) == false) {
+    /* Graph isn't connected, single run isn't possible. */
+    return false;
+  }
+
+  /* Check if topological sort is a valid run */
+  return is_topological_a_run(topological_sort, adj_list, n);
+}
+
+Deque** read_in_input(int *n) {
+  int u, v;               // From vertex u to v
+  int first = true;       // Is it the first line?
+  int temp1, temp2;       // Temporarily holds the read values
+  int i;
+  Deque **adj_list;
+
+  /* Reads in all the vertices and edges into adj_list */
+  while((scanf("%d %d", &temp1, &temp2)) != EOF) {
+
+    /* Read in number of vertices and edges and allocate adj_list */
+    if (first) {
+
+      first = false;
+
+      *n = temp1 + NUM_ROOTS;
+
+      adj_list = malloc((*n)*sizeof(**adj_list));
+      assert(adj_list != NULL);
+
+      for(i = 0; i < *n; i++) {
+        adj_list[i] = new_deque();
+        assert(adj_list[i] != NULL);
+      }
+
+    }
+    else {
+      u = temp1;
+      v = temp2;
+      deque_push(adj_list[u], v);
+    }
+  }
+
+  return adj_list;
+}
+
+bool topological_DFS(Deque **adj_list, Deque *topological_sort, int n) {
+
+  /* Takes in an adjacency list, performs a depth-first search, keeps track */
+  /* of popped values in a topological_sort Deque. Also takes in said */
+  /* topological_sort Deque and an integer n which holds amount of vertices. */
+
+  /* Returns false if graph isn't connected which automatically makes a */
+  /* single run impossible. Otherwise returns true. */
+
+  /* Holds whether we've explored a node */
+  int* explored = malloc(n*sizeof(*explored));
+  int i;
+  int count = 0;     // How many nodes have we explored?
+  for(i = 0; i < n; i++) {
+    explored[i] = UNEXPLORED;
+  }
+
+  /* Utilise a deque as a stack by only using push() and pop() */
+  Deque *DFS_stack = new_deque();
+
+  /* Explore the root node */
+  DFS_explore(ROOT_NODE, adj_list, explored, DFS_stack, topological_sort,
+              &count);
+
+  free(explored);
+  /* Individual nodes were freed in the popping process */
+  free(DFS_stack);
+
+  /* Will return false if graph isn't connected */
+  if(count != n) {
+    return false;
+  }
+  return true;
+}
+
+void DFS_explore(Data vertex, Deque **adj_list, int* explored, Deque *DFS_stack,
+                 Deque *topological_sort, int *count) {
+
+  /* We've now seen this node and we need to mark this point so we can
+  backtrack */
+  deque_push(DFS_stack, vertex);
+  explored[vertex] = ++(*count);
+
+  /* Explore all sub-vertices of this vertex */
+  Node* temp = adj_list[vertex]->top;
+  while(temp != NULL) {
+      Data n_vertex = temp->data;
+      if (explored[n_vertex] == UNEXPLORED) {
+          DFS_explore(n_vertex, adj_list, explored, DFS_stack, topological_sort,
+                      count);
+      }
+      temp = temp->next;
+  }
+
+  /* Finished exploring the vertex, put it in the topological sort and forget */
+  deque_push(topological_sort, deque_pop(DFS_stack));
+}
+
+bool is_topological_a_run(Deque *topological_sort, Deque **adj_list, int n) {
+
+  /* Takes in a topological_sort Deque as well as an adjacency list. */
+  /* Goes through the topological_sort's pairs of adjacent vertices */
+  /* (u,v) and ensures there exists an edge u->v in the adjacency list. */
+  /* Returns false if a single edge doesn't exist, otherwise returns true. */
+
+  /* temp_topo_u/v traverse the topological sort and find u/v respectively */
+  Node *temp_topo_u = topological_sort->top;
+  Node *temp_topo_v = topological_sort->top->next;
+
+  /* Vertices u and v, and a boolean value of if an edge u->v exists */
+  Data u,v;
+  bool edge;
+
+  /* Traverse the topological sort list and ensure each edge u->v exists */
+  while(temp_topo_v != NULL) {
+    u = temp_topo_u->data;
+    v = temp_topo_v->data;
+
+    edge = false;
+
+    /* Explores all the edges of a vertex u */
+    Node *temp_edges = adj_list[u]->top;
+    while(temp_edges != NULL) {
+
+      if(temp_edges->data == v) {
+        edge = true;
+      }
+
+      temp_edges = temp_edges->next;
+    }
+
+    /* No edge between u->v, no single run possible */
+    if(edge == false) {
+      free_deque(topological_sort);
+      return false;
+    }
+
+    temp_topo_u = temp_topo_u->next;
+    temp_topo_v = temp_topo_v->next;
+  }
+
+  free_deque(topological_sort);
+  free_adj_list(adj_list, n);
+  return true;
+}
+
+void free_adj_list(Deque **adj_list, int n) {
+  /* Takes in the adjacency list and number of vertices and frees all */
+  /* the memory associated. */
+
+  int i;
+  for(i = 0; i < n; i++) {
+    free_deque(adj_list[i]);
+  }
+
+  free(adj_list);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
